3539e87b0940389ddc1e5d869caa930ec2e8b4b2
samocvetov <samocvetov@gmail.com>
2026-01-30 17:24:41 +0300

    w.ps1

--- w.ps1
+++ w.ps1
@@ -1,24 +1,23 @@
 $apps = @("7zip.7zip", "Notepad++.Notepad++", "RustDesk.RustDesk", "AnyDesk.AnyDesk", "VideoLAN.VLC", "PDFgear.PDFgear", "Google.Chrome.EXE", "Telegram.TelegramDesktop", "9NKSQGP7F2NH", "Zoom.Zoom", "Yandex.Browser", "Yandex.Messenger", "AdrienAllard.FileConverter", "alexx2000.DoubleCommander", "WinDirStat.WinDirStat")
 
-Write-Host "Обновление системы..." -ForegroundColor Cyan
+Write-Host "--- Updating System ---" -ForegroundColor Cyan
 winget upgrade --all --silent --accept-source-agreements --accept-package-agreements | Out-Null
 
-Write-Host "Установка программ:" -ForegroundColor Cyan
+Write-Host "--- Installing Packages ---" -ForegroundColor Cyan
 
 foreach ($app in $apps) {
-    # Пытаемся установить, подавляя весь вывод самого winget
-    $process = winget install --id $app --silent --accept-source-agreements --accept-package-agreements 2>&1 | Out-Null
+    $null = winget install --id $app --silent --accept-source-agreements --accept-package-agreements
     
     if ($LASTEXITCODE -eq 0) {
         Write-Host "[ OK ] $app" -ForegroundColor Green
     } elseif ($LASTEXITCODE -eq -1978335189) {
-        Write-Host "[SKIP] $app (уже есть)" -ForegroundColor Gray
+        Write-Host "[SKIP] $app (already installed)" -ForegroundColor Gray
     } else {
-        Write-Host "[FAIL] $app (Код: $LASTEXITCODE)" -ForegroundColor Red
+        Write-Host "[FAIL] $app (Code: $LASTEXITCODE)" -ForegroundColor Red
     }
 }
 
-Write-Host "`nГотово!" -ForegroundColor Cyan
+Write-Host "`nDone!" -ForegroundColor Cyan
 
-# Самоудаление (если файл существует)
+# Self-cleanup
 if (Test-Path $PSCommandPath) { Remove-Item $PSCommandPath -Force }
