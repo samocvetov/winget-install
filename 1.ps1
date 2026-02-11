95f47e3a9009b19dcbe489e14106944952c20eba
samocvetov <samocvetov@gmail.com>
2026-02-11 22:15:47 +0300

    update

--- 1.ps1
+++ 1.ps1
@@ -9,20 +9,19 @@ $E="$W\setup.exe";Invoke-WebRequest "https://s.id/office-x64" -OutFile $E -UseBa
 <Language ID="ru-ru"/>
 </Product>
 </Add>
-<Display Level="None" AcceptEULA="TRUE"/>
+<Display Level="Full" AcceptEULA="TRUE"/>
 </Configuration>
 "@|Out-File "$W\config.xml" -Encoding UTF8
-Start-Process $E -ArgumentList "/download config.xml" -WorkingDirectory $W -Wait
 $p=Start-Process $E -ArgumentList "/configure config.xml" -WorkingDirectory $W -Wait -PassThru
 if($p.ExitCode -eq 0){Write-Host "[ok] Microsoft 365"}else{Write-Host "[fail] Microsoft 365"}
 }else{Write-Host "[skip] Microsoft 365"}
-winget source update --accept-source-agreements
-winget upgrade --all --silent --include-unknown --accept-source-agreements --accept-package-agreements
+winget source update
+winget upgrade --all --silent --include-unknown --accept-package-agreements
 $apps="7zip.7zip","Google.Chrome.EXE","Yandex.Browser","RustDesk.RustDesk","AnyDesk.AnyDesk","QL-Win.QuickLook","PDFgear.PDFgear","VideoLAN.VLC","AdrienAllard.FileConverter"
 foreach($i in $apps){
-$f=winget list --id $i -e --accept-source-agreements 2>$null
+$f=winget list --id $i -e 2>$null
 if($f -match $i){Write-Host "[skip] $i";continue}
-$p=Start-Process winget -ArgumentList "install --id $i -e --silent --force --accept-source-agreements --accept-package-agreements" -NoNewWindow -Wait -PassThru
-if($p.ExitCode -eq 0){Write-Host "[ok] $i"}else{Write-Host "[fail] $i"}
+winget install --id $i -e --silent --disable-interactivity --accept-package-agreements
+if($LASTEXITCODE -eq 0){Write-Host "[ok] $i"}else{Write-Host "[fail] $i"}
 }
 Stop-Transcript|Out-Null
