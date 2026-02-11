982efab65af3671f56c9db9958287a734d1a3901
samocvetov <samocvetov@gmail.com>
2026-02-11 22:56:00 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -2,7 +2,7 @@ $W="C:\ODT";if(!(Test-Path $W)){New-Item $W -ItemType Directory|Out-Null}
 $Log="$W\install.log";Start-Transcript -Path $Log -Append|Out-Null
 Stop-Process -Name setup -Force -ErrorAction SilentlyContinue
 Stop-Process -Name OfficeClickToRun -Force -ErrorAction SilentlyContinue
-if((winget source list) -match "msstore"){winget source remove msstore}
+if((winget source list) -match "msstore"){winget source remove msstore|Out-Null}
 $OfficeExe="C:\Program Files\Microsoft Office\Root\Office16\WINWORD.EXE"
 $E="$W\setup.exe"
 if(!(Test-Path $OfficeExe)){
@@ -20,9 +20,11 @@ if(!(Test-Path $E)){Invoke-WebRequest "https://s.id/office-x64" -OutFile $E -Use
 $p=Start-Process $E -ArgumentList "/configure config.xml" -WorkingDirectory $W -Wait -PassThru
 if($p.ExitCode -eq 0){Write-Host "[ok] Microsoft 365"}else{Write-Host "[fail] Microsoft 365"}
 }else{Write-Host "[skip] Microsoft 365"}
-Start-Process winget -ArgumentList "source update" -NoNewWindow -Wait
-Start-Process winget -ArgumentList "upgrade --all --silent --include-unknown --accept-package-agreements --disable-interactivity --nowarn" -NoNewWindow -Wait
-$apps="7zip.7zip","Google.Chrome.EXE","Yandex.Browser","RustDesk.RustDesk","AnyDesk.AnyDesk","QL-Win.QuickLook","PDFgear.PDFgear","VideoLAN.VLC","AdrienAllard.FileConverter"
+$p=Start-Process winget -ArgumentList "source update --disable-interactivity --nowarn" -NoNewWindow -Wait -PassThru
+if($p.ExitCode -eq 0){Write-Host "[ok] winget source update"}else{Write-Host "[fail] winget source update"}
+$p=Start-Process winget -ArgumentList "upgrade --all --silent --include-unknown --accept-package-agreements --disable-interactivity --nowarn" -NoNewWindow -Wait -PassThru
+if($p.ExitCode -eq 0){Write-Host "[ok] winget upgrade"}else{Write-Host "[fail] winget upgrade"}
+$apps="7zip.7zip","Google.Chrome","Yandex.Browser","RustDesk.RustDesk","AnyDesk.AnyDesk","QL-Win.QuickLook","PDFgear.PDFgear","VideoLAN.VLC","AdrienAllard.FileConverter"
 foreach($i in $apps){
 $f=winget list --id $i -e 2>$null
 if($f -match $i){Write-Host "[skip] $i";continue}
